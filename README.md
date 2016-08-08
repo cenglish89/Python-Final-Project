0f78cf6663d3873c8c3bcb9727bced05583e7586
Christina <student@pyclassvm>
2016-08-08 19:00:15 -0400

    Adding website to github

---
+++ README.md
@@ -0,0 +1,3 @@
+#Website Project Example
+
+This website is part of my final project for a Johns Hopkins course in Python to analyze data and build a website to showcase the skills I learned.
---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/results')
+def results():
+	return render_template('results.html')
+
+@app.route('/research')
+def research():
+	return render_template('researchq.html')
+
+@app.route('/project')
+def project():
+	return render_template('project.html')
+
+@app.route('/methodology')
+def methodology():
+	return render_template('methodology.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
